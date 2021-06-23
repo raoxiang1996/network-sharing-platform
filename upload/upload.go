99db6818ee4e74f635e2a71c27a1f8196e7cd51d
raoxiang <raoxiang1996@gmail.com>
2021-06-23 22:26:19 +0800

    调整头文件，按照标准库，第三方库，本地三个顺序

--- upload/upload.go
+++ upload/upload.go
@@ -1,14 +1,15 @@
 package upload
 
 import (
-	"University-Information-Website/utils"
-	"University-Information-Website/utils/errmsg"
 	"context"
 	"fmt"
 	"mime/multipart"
 
 	"github.com/qiniu/go-sdk/v7/auth/qbox"
 	"github.com/qiniu/go-sdk/v7/storage"
+
+	"University-Information-Website/utils"
+	"University-Information-Website/utils/errmsg"
 )
 
 func Upload(file multipart.File, fileSize int64) (string, int) {
